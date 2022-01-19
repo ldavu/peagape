83f16abf71a86c61db7f118b7d6d89372f5ff1a8
Davu <41762352+ldavu@users.noreply.github.com>
2022-01-19 20:28:55 -0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,39 @@
+<?php
+/*
+$usuario = "Davi";
+$senha = 123;
+
+if($senha === 123) && ($usuario === "Davi")
+{
+  echo "logado";
+  
+}
+else
+{
+  echo "inválido";
+}
+*/
+$a = 203;
+
+switch($a)
+{
+  case 10:
+    echo "é 10!";
+  break;
+
+  case 20:
+    echo "é 20";
+  break;
+  
+  case 30:
+    echo "é 30";
+  break;
+  
+  default:
+    echo "Nenhum";
+  break;
+  
+}
+
+
+?>
